cac17faa54bea24545a5138e5648c93366018959
Marek Abel <marek-abel@wp.pl>
2026-05-30 23:25:01 +0200

    Dodaje w artości przykładowe w serwisie

--- MediSenior/Services/FirebaseConfig.cs
+++ MediSenior/Services/FirebaseConfig.cs
@@ -2,9 +2,9 @@ namespace MediSenior.Services;
 
 public static class FirebaseConfig
 {
-    public const string ApiKey = "AIzaSyCEbxxlCPA5xNCJ4N-4OVg1264lMPtFltM";
+    public const string ApiKey = "YOUR_FIREBASE_API_KEY";
 
-    public const string ProjectId = "medisenior-de333";
+    public const string ProjectId = "YOUR_FIREBASE_PROJECT_ID";
 
     public static string FirestoreBaseUrl =>
         $"https://firestore.googleapis.com/v1/projects/{ProjectId}/databases/(default)/documents";
